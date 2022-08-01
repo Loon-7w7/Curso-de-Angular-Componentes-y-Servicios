0c09f0c176f8376d18a40d1578a6d853b51e99e3
Luis Ku <99504287+Loon-7w7@users.noreply.github.com>
2022-08-01 11:22:07 -0500

    Cambios video 11

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import{HttpClient,HttpParams} from '@angular/common/http';
 import { Product,CreateProductDTO,UpdateProductDTO } from '../Models/prodcut.module';
+import { retry } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  private apiUrl = 'https://young-sands-07814.herokuapp.com/api/products';
+  private apiUrl = '/api/products';
   constructor(
     private http: HttpClient
   ) { }
@@ -17,7 +18,10 @@ export class ProductsService {
       params = params.set('limit', limit);
       params = params.set('offset', limit);
     }
-    return this.http.get<Product[]>(this.apiUrl, { params });
+    return this.http.get<Product[]>(this.apiUrl, { params })
+    .pipe(
+      retry(3)
+    );
   }
   getProduct(id: string) {
     return this.http.get<Product>(`${this.apiUrl}/${id}`)
